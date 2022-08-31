1b54e575122c85afe07142bab8b208dcccc37374
poeticinjustice <poeticinjustice@gmail.com>
2022-08-30 22:31:53 -0400

    last update for heroku

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -4,9 +4,32 @@ const About = () => {
   return (
     <div>
       <h1>About this app</h1>
-      <p className='my-1'>Get yer MailChimp stats</p>
-      <p className='bg-dark p'>
+      <p>
         <strong>Version 0.7</strong>
+      </p>
+      <br></br>
+      <p>
+        <strong>
+          <a
+            href='https://mailchamp2019.herokuapp.com/'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            2019 site
+          </a>
+        </strong>
+      </p>
+      <br></br>
+      <p>
+        <strong>
+          <a
+            href='https://mailchamp2019.herokuapp.com/'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            https://mailchamp2019.herokuapp.com/
+          </a>
+        </strong>
       </p>
     </div>
   )
